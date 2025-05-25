9f089847e35f12e08c6ffa3abb5b61284d79c4a4
Alll Rafsann <141483613+al-hasby@users.noreply.github.com>
2025-05-25 14:43:34 +0700

    Add files via upload

---
+++ fungsi2.js
@@ -0,0 +1,10 @@
+function navMobile(){
+    var x = document.getElementById("myNav");
+    if(x.className ==="top-nav"){
+        x.className = "respon";
+
+    }else{
+        x.className="top-nav";
+    }
+
+}
